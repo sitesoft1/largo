b7f474f8bf239ab3d9e2be777b5e16d9860ae260
sitesoft1 <sitesoft1@gmail.com>
2021-02-18 06:15:40 +0200

    \Вывел менюшку с детьми и колонками, осталось вывести слайды

--- catalog/controller/common/menu.php
+++ catalog/controller/common/menu.php
@@ -49,7 +49,9 @@ class ControllerCommonMenu extends Controller {
             if($slidemenues){
                 $data['slidemenu'] = array();
                 foreach ($slidemenues as $item_arr){
-                    
+    
+                    $column = 1;
+                    $children_data = array();
                     $type = $item_arr['type'];
                     $sources = unserialize($item_arr['source']);
                     $source = $sources[$type];
@@ -61,8 +63,25 @@ class ControllerCommonMenu extends Controller {
         
                     if($type == 'category_url'){
                         $href = $this->url->link('product/category', 'path=' . $source['category_id']);
+                        
+                        //Получим категорию чтобы узнать параметр column
+                        $category = $this->model_catalog_category->getCategory($source['category_id']);
             
-                        //children...
+                        //children
+                        // Level 2
+                        $children_data = array();
+                        $children = $this->model_catalog_category->getCategories($source['category_id']);
+                        foreach ($children as $child) {
+                            $filter_data = array(
+                                'filter_category_id'  => $child['category_id'],
+                                'filter_sub_category' => true
+                            );
+                            $children_data[] = array(
+                                'name'  => $child['name'] . ($this->config->get('config_product_count') ? ' (' . $this->model_catalog_product->getTotalProducts($filter_data) . ')' : ''),
+                                'href'  => $this->url->link('product/category', 'path=' . $source['category_id'] . '_' . $child['category_id'])
+                            );
+                        }
+                        //children END
             
                     }elseif ($type == 'information_url'){
                         $href = $this->url->link('information/information', 'information_id=' .  $source['information_id']);
@@ -73,9 +92,9 @@ class ControllerCommonMenu extends Controller {
                     // Level 1
                     $data['slidemenu'][] = array(
                         'name'     => $slidemenu_name,
-                        'image'     => $image,
-                        'children' => array(),//...
-                        'column'   => 1,
+                        'image'    => $image,
+                        'children' => $children_data,
+                        'column'   => $category['column'] ? $category['column'] : 1,
                         'href'     => $href
                     );
                 }
